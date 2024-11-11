b690b1b4b7d677e34634b4d0d5221dd642289c4d
Cristian-svg598 <criguies@gmail.com>
2024-11-11 11:14:03 +0000

    Crear Funcion generarNombresDominio

--- src/app.js
+++ src/app.js
@@ -9,3 +9,30 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+const pronList = new Array("yo", "tu", "el", "nosotros", "vosotros", "ellos");
+const adjList = new Array("Optimo", "Afortunado", "Amarillo", "Alto");
+const sustList = new Array("Perretes", "Gatetes", "Formula1");
+const final = new Array(".com", ".es", ".edu", ".net");
+
+function generarNombresDominio(pronList, adjList, sustList) {
+  pronList.forEach(pronombre => {
+    adjList.forEach(adjetivo => {
+      sustList.forEach(sustantivo => {
+        final.forEach(final => {
+          const extension = final.replace(".", "");
+          if (sustantivo.endsWith(extension)) {
+            const sustantivoRecortado = sustantivo.slice(0, -extension.length);
+            console.log(
+              `${pronombre}${adjetivo}${sustantivoRecortado}${final}`
+            );
+          } else {
+            console.log(`${pronombre}${adjetivo}${sustantivo}${final}`);
+          }
+        });
+      });
+    });
+  });
+}
+
+generarNombresDominio(pronList, adjList, sustList);
